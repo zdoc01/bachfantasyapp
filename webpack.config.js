deddb80ed1bda21704cb871a278084e4a53b0020
Zachary Lumpkins <zdoc01@gmail.com>
2016-10-15 16:31:04 -0700

    fix webpack publicPath and asset src urls

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'server', 'public', 'js'),
     filename: 'bundle.js',
-    publicPath: '/server/public/'
+    publicPath: '/public/'
   },
   module: {
     loaders: [
